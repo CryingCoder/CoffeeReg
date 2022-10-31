27f17f78cf64e4563be9f628140ddf4324984040
CryingCoder <112829574+CryingCoder@users.noreply.github.com>
2022-10-30 20:01:46 -0400

    Submit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using CoffeeShopRegi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoffeeShopRegi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+                
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RegistrationForm()
+        {
+            return View();
+        }
+
+        public IActionResult Register(Registration r)
+        {
+            return View(r);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Registration.cs
@@ -0,0 +1,13 @@
+﻿using CoffeeShopRegi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopRegi.Models
+{
+    public class Registration
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
